6840bda22002076b1b4820b4150b15fd96111cc5
Alex Souza <alexsouza.notorio@gmail.com>
2025-11-20 08:50:32 -0300

    Instalando e configurando Angular v20

--- UI/src/app/app.routes.ts
+++ UI/src/app/app.routes.ts
@@ -1,28 +1,3 @@
-// src/app/app.routes.ts
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [
-  { path: '', redirectTo: '/frota', pathMatch: 'full' },
-  {
-    path: 'frota',
-    loadComponent: () =>
-      import('./modules/gestao-frota/components/lista-veiculos/lista-veiculos.component').then(
-        (c) => c.ListaVeiculosComponent
-      ),
-  },
-  {
-    path: 'cadastro-veiculo',
-    loadComponent: () =>
-      import('./modules/gestao-frota/components/cadastro-veiculo/veiculo-form.component').then(
-        (c) => c.VeiculoFormComponent
-      ),
-  },
-  {
-    path: 'cadastro-motorista',
-    loadComponent: () =>
-      import('./modules/gestao-frota/components/cadastro-motorista/cadastro-motorista').then(
-        (c) => c.CadastroMotoristaComponent
-      ),
-  },
-  { path: '**', redirectTo: '/frota' },
-];
+export const routes: Routes = [];
--- UI/src/main.ts
+++ UI/src/main.ts
@@ -1,6 +1,6 @@
-// src/main.ts
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { App } from './app/app';
 
-bootstrapApplication(AppComponent, appConfig).catch((err: any) => console.error(err));
+bootstrapApplication(App, appConfig)
+  .catch((err) => console.error(err));
