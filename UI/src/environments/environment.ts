8bba2ab076463c07bd5575b4632227e53692f6f9
Alex Souza <alexsouza.notorio@gmail.com>
2025-11-23 14:55:06 -0300

    update arquivos

--- UI/src/environments/environment.ts
+++ UI/src/environments/environment.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.ts
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5165/api',
+  apiUrl: 'https://frotamaster-production.up.railway.app/api',
 };
