90d904f627073e62b5ad39f8fcc0fe73249286e8
Alex Souza <alexsouza.notorio@gmail.com>
2025-11-23 18:58:00 -0300

    Melhorias no Layout

--- UI/src/app/app.ts
+++ UI/src/app/app.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './navbar/navbar';
+import { NavbarComponent } from './layout/navbar/navbar';
+import { FooterComponent } from './layout/footer/navbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   template: `
     <app-navbar></app-navbar>
-    <router-outlet></router-outlet>
+
+    <div class="min-h-screen">
+      <router-outlet></router-outlet>
+    </div>
+
+    <app-footer></app-footer>
   `,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
 })
 export class App {}
