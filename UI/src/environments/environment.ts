f7e9b30f7dbd250dd6fe060992bd715c40fd6fdf
Alex Souza <alexsouza.notorio@gmail.com>
2025-11-30 10:40:11 -0300

    mudando rota do banco

--- UI/src/environments/environment.ts
+++ UI/src/environments/environment.ts
@@ -1,5 +1,6 @@
 // src/environments/environment.ts
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5165/api',
+  //apiUrl: 'http://localhost:5165/api',
+  apiUrl: 'https://frotamaster-production.up.railway.app/api',
 };
